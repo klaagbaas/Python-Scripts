080080ada6ed3388727eb752447808aa75326aef
klaagbaas <64561028+klaagbaas@users.noreply.github.com>
2022-04-25 00:15:47 +0200

    Update and rename guess nr.py to Introduction to CS MIT/Guess the Number Game.py

--- Introduction to CS MIT/Guess the Number Game.py
+++ Introduction to CS MIT/Guess the Number Game.py
@@ -11,16 +11,16 @@ correct = 0
 
 print("Please think of a number between 0 and 100!")
 while correct == 0:
-    ans = int((high+low)/2)
-    print("Is your secret number", ans,"?")
+    guess = int((high+low)/2)
+    print("Is your secret number", guess,"?")
     print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.", end = ' ')
     answer = input()
     if answer == "c":
         correct += 1
     elif answer == "l":
-        low = ans
+        low = guess
     elif answer == "h":
-        high = ans
+        high = guess
     else:
         print("Sorry, I did not understand your input.")
-print("Game over. Your secret number was:", ans)
+print("Game over. Your secret number was:", guess)
