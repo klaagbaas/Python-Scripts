e43f42bafa416972228f056452b8f93eea6e748c
klaagbaas <64561028+klaagbaas@users.noreply.github.com>
2022-05-06 10:20:20 +0200

    Update selectivecopy.py
    This version only copies non-main (-001) images. It skips copying any files that are already in the destination directory

--- File and Directory Actions/selectivecopy.py
+++ File and Directory Actions/selectivecopy.py
@@ -1,20 +1,29 @@
 '''
 scan source folder and subfolder contents
-if filepath contains 'landmark', copy *-001.jpg to destination folder
+if filepath contains 'landmark', name doesn't end with *-001.jpg
+and name isn't in dest folder,copy file to destination folder
 '''
 
 import shutil
 import os
 
-source = '/Volumes/GoogleDrive/Shared drives/US Descon 2021 Master/Snapwire/'
-dest = '/Volumes/GoogleDrive/Shared drives/US Descon 2021 Master/_LANDMARK_MAINS'
+source = '/Volumes/GoogleDrive/Shared drives/US Descon 2021 Master/Shutterstock/1. PHOTO'
+dest = '/Volumes/GoogleDrive/Shared drives/US Descon 2021 Master/_LANDMARK_ADDITIONAL'
 dirname = 'Landmarks'
-filepath = []
-for root, dirs, files in os.walk(source):
+filepath_source = []
+filepath_dest = []
+for root, dirs, files in os.walk(source): #scan files in source directory
     for name in files:
-        filepath.append(os.path.join(root, name))
+        filepath_source.append(os.path.join(root, name))
+        
+for root, dirs, files in os.walk(dest): #scan files in destination directory
+    for name in files:
+        filepath_dest.append(os.path.join(root, name))
 
-for name in filepath:
+for name in filepath_source:
     if dirname in name:
-        if '-001' in name:
-            shutil.copy(name, dest)
+        if '-001' not in name:
+            if name not in filepath_dest: #skip files already in the destination directory
+                shutil.copy(name, dest)
+                print(name)
+print("done")
